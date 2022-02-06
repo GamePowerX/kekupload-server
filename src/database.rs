689b4ddff50c4c8f1d7a6440e41591b5ecdbd8bf
KekOnTheWorld <kekontheworld@gmail.com>
2022-02-06 13:13:04 +0100

    Please help

---
+++ src/database.rs
@@ -0,0 +1,15 @@
+use diesel::pg::PgConnection;
+
+use diesel::r2d2::{ Pool, PooledConnection, ConnectionManager, PoolError };
+
+pub type PgPool = Pool<ConnectionManager<PgConnection>>;
+pub type PgPooledConnection = PooledConnection<ConnectionManager<PgConnection>>;
+
+fn init_pool(database_url: &str) -> Result<PgPool, PoolError> {
+    let manager = ConnectionManager::<PgConnection>::new(database_url);
+    Pool::builder().build(manager)
+}
+
+pub fn establish_connection(database_url: String) -> PgPool {
+    init_pool(&database_url).expect("Failed to create pool")
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+
+use std::io;
+
+use std::collections::HashMap;
+
+use dotenv::dotenv;
+use std::env;
+//use rocket::tokio::time::{sleep, Duration};
+use rocket::config::LogLevel;
+use rocket::State;
+
+use tokio::fs::File;
+use tokio::io::AsyncWriteExt;
+
+use md5::{Md5, Digest};
+
+mod database;
+mod random;
+
+struct UploadState {
+    map: HashMap<String, UploadEntry>
+}
+
+struct UploadEntry {
+    file: File,
+    ext: String,
+    hasher: Md5
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "KekUpload api made by KekOnTheWorld!"
+}
+
+#[get("/c/<ext>")]
+async fn create(ext: String, state: &State<UploadState>) -> io::Result<String> {
+    let id = random::random_b64(64);
+    let file = File::create("tmp/".to_owned() + &id).await?;
+    let hasher = Md5::new();
+
+    let entry = UploadEntry { file, ext, hasher };
+
+    println!("Created stream with ID: {}", &id);
+
+    state.map.insert(id.clone(), entry);
+    
+    return Ok(id);
+}
+
+#[launch]
+fn rocket() -> _ {
+    dotenv().ok();
+
+    let base = env::var("base").unwrap_or("/".to_string());
+
+    let port = env::var("port")
+        .unwrap_or("8000".to_string())
+        .parse()
+        .unwrap_or(8000);
+
+    database::establish_connection(env::var("dburl").expect("Missing dburl in .env"));
+
+    let figment = rocket::Config::figment()
+        .merge(("log_level", LogLevel::Critical))
+        .merge(("port", port));
+
+    println!("http://localhost:{}{}", port, base);
+
+    rocket::custom(figment)
+        .manage(UploadState { map: HashMap::new() })
+        .mount(base, routes![index, create])
 }
