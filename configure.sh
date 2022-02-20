7e59b13db8b7c0fd77920a06649065626e393748
KekOnTheWorld <kekontheworld@gmail.com>
2022-02-20 14:52:25 +0100

    Fixed configure.sh

--- configure.sh
+++ configure.sh
@@ -29,7 +29,7 @@ db_url="postgres://"$db_username":"$db_password"@"$db_host":"$db_port"/"$db_data
 
 printf $green"\nThe database url is set to: "$orange$db_url"\n\n"
 
-echo "DATABASE_URL=$db_url" >> $dotenv
+echo "DATABASE_URL='$db_url'" >> $dotenv
 
 printf $orange"Running diesel migrations...\n"$reset
 diesel migration run || exit -1
@@ -40,33 +40,33 @@ printf $green"\nSuccess!\n\n"
 printf $yellow"Please enter the port (6942): "$cyan
 read port
 
-echo "port=$port" >> $dotenv
+echo "port='$port'" >> $dotenv
 
 # api_base
 printf $yellow"Please enter the api base url (/api/): "$cyan
 read api_base
 
-echo "api_base=$api_base" >> $dotenv
+echo "api_base='$api_base'" >> $dotenv
 
 # chunk_size
 printf $yellow"Please enter the max chunk size in KiB (2048): "$cyan
 read chunk_size
 
-echo "chunk_size=$chunk_size" >> $dotenv
+echo "chunk_size='$chunk_size'" >> $dotenv
 
 # upload_dir
 printf $yellow"Please enter the upload directory location (upload/): "$cyan
 read upload_dir
-mkdir -p $upload_dir
+mkdir -p "$upload_dir"
 
-echo "upload_dir=$upload_dir" >> $dotenv
+echo "upload_dir='$upload_dir'" >> $dotenv
 
 # tmp_dir
 printf $yellow"Please enter the directory location for temporary files (tmp/): "$cyan
 read tmp_dir
-mkdir -p $tmp_dir
+mkdir -p "$tmp_dir"
 
-echo "tmp_dir=$tmp_dir" >> $dotenv
+echo "tmp_dir='$tmp_dir'" >> $dotenv
 
 
 printf $orange"\nFrontend\n\n"
@@ -84,15 +84,15 @@ if [ "$frontend" != "none" ]; then
     rm -rf $dir
 
     # web_dir
-    echo "web_dir=web/" >> $dotenv
+    echo "web_dir='web/'" >> $dotenv
     # web_host
-    echo "web_host=true" >> $dotenv
+    echo "web_host='true'" >> $dotenv
 
     # web_base
     printf $yellow"Please enter the web base (/): "$cyan
     read web_base
 
-    echo "web_base=$web_base" >> $dotenv
+    echo "web_base='$web_base'" >> $dotenv
 fi
 
 
@@ -102,28 +102,28 @@ printf $orange"\nEmbeds\n\n"
 printf $yellow"Please enter the embed description (File uploaded to UploadServer): "$cyan
 read embed_description
 
-echo "embed_description=$embed_description" >> $dotenv
+echo "embed_description='$embed_description'" >> $dotenv
 
 # embed_color
 printf $yellow"Please enter the embed color (#ffffff): "$cyan
 read embed_color
 
-echo "embed_color=$embed_color" >> $dotenv
+echo "embed_color='$embed_color'" >> $dotenv
 
 # download_url
 printf $yellow"Please enter the download url (http://localhost:6942/api/d/): "$cyan
 read download_url
 
-echo "download_url=$download_url" >> $dotenv
+echo "download_url='$download_url'" >> $dotenv
 
 # embed_route_base
 printf $yellow"Please enter the embed route (/): "$cyan
 read embed_route_base
 
-echo "embed_route_base=$embed_route_base" >> $dotenv
+echo "embed_route_base='$embed_route_base'" >> $dotenv
 
 # download_route_base
 printf $yellow"Please enter the download route (/): "$cyan
 read download_route_base
 
-echo "download_route_base=$download_route_base" >> $dotenv
+echo "download_route_base='$download_route_base'" >> $dotenv
