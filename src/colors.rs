121bd592e3804f6395471ca23917bedfc0b401f4
KekOnTheWorld <kekontheworld@gmail.com>
2022-05-04 18:13:00 +0200

    Added colors

---
+++ src/colors.rs
@@ -0,0 +1,14 @@
+pub const RED: &'static str = "\x1B[38;2;255;0;0m";
+pub const LIGHT_RED: &'static str = "\x1B[38;2;255;150;150m";
+pub const GREEN: &'static str = "\x1B[38;2;0;255;0m";
+pub const LIGHT_GREEN: &'static str = "\x1B[38;2;150;255;150m";
+pub const BLUE: &'static str = "\x1B[38;2;17;84;252m";
+pub const LIGHT_BLUE: &'static str = "\x1B[38;2;101;142;252m";
+pub const ORANGE: &'static str = "\x1B[38;2;252;149;5m";
+pub const LIGHT_ORANGE: &'static str = "\x1B[38;2;252;181;83m";
+pub const YELLOW: &'static str = "\x1B[38;2;252;241;27m";
+pub const LIGHT_YELLOW: &'static str = "\x1B[38;2;252;242;133m";
+
+pub const RESET: &'static str = "\x1B[0m";
+pub const CURSOR_UP: &'static str = "\x1B[A1";
+pub const CLEAR_LINE: &'static str = "\x1B[K";
